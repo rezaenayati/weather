eda7012549d3e6e3d0d19f85e8dfcee3a6611dd9
Reza Enayati <enayatii.reza@gmail.com>
2019-05-03 00:32:04 +0430

    Add text input

--- App.js
+++ App.js
@@ -4,16 +4,25 @@ import {
   Text,
   View,
   Platform,
+  TextInput,
+  KeyboardAvoidingView,
 } from 'react-native';
 
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
+      <KeyboardAvoidingView style={styles.container} behavior='padding'>
         <Text style={[styles.largeText , styles.textStyle]}>San Francisco</Text>
         <Text style={[styles.smallText , styles.textStyle]}>Light Cloud</Text>
         <Text style={[styles.largeText , styles.textStyle]}>24°</Text>
-      </View>
+        <TextInput
+          autoCorrect={false}
+          placeholder='Search any city'
+          placeholderTextColor='white'
+          style={styles.textInput}
+          clearButtonMode= 'always'
+        />
+      </KeyboardAvoidingView>
     );
   }
 }
@@ -27,12 +36,29 @@ const styles = StyleSheet.create({
   },
   textStyle: {
     textAlign: 'center',
-    fontFamily: Platform.OS === 'ios' ? 'AvenirNext-Regular' : 'Roboto',
+    ...Platform.select({
+      ios: {
+        fontFamily: 'AvenirNext-Regular',
+      },
+      android: {
+        fontFamily: 'Roboto',
+      },
+    }),
   },
   largeText: {
     fontSize: 44,
   },
   smallText: {
     fontSize: 18,
+  },
+  textInput: {
+    backgroundColor: '#666',
+    color: 'white',
+    height: 40,
+    width: 300,
+    marginTop: 20,
+    marginHorizontal: 20,
+    paddingHorizontal: 10,
+    alignSelf: 'center',
   },
 });
