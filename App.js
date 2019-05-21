089fd1de117ace640d0973a4c80328efd230a7ab
Reza Enayati <enayatii.reza@gmail.com>
2019-05-22 02:49:20 +0430

    set backgroundImage

--- App.js
+++ App.js
@@ -6,18 +6,29 @@ import {
   Platform,
   TextInput,
   KeyboardAvoidingView,
+  ImageBackground,
 } from 'react-native';
 
+import getImageForWeather from './utils/getImageForWeather';
 import SearchInput from './components/SearchInput';
 
 export default class App extends React.Component {
   render() {
     return (
       <KeyboardAvoidingView style={styles.container} behavior='padding'>
-        <Text style={[styles.largeText , styles.textStyle]}>San Francisco</Text>
-        <Text style={[styles.smallText , styles.textStyle]}>Light Cloud</Text>
-        <Text style={[styles.largeText , styles.textStyle]}>24°</Text>
-        <SearchInput placeholder="Search any city" />
+        <ImageBackground
+          source={getImageForWeather('Snow')}
+          style={styles.imageContainer}
+          imageStyle={styles.image}
+        >
+          <View style={styles.detailsContainer}>
+            <Text style={[styles.largeText , styles.textStyle]}>San Francisco</Text>
+            <Text style={[styles.smallText , styles.textStyle]}>Light Cloud</Text>
+            <Text style={[styles.largeText , styles.textStyle]}>24°</Text>
+            <SearchInput placeholder="Search any city" />
+          </View>
+
+        </ImageBackground>
       </KeyboardAvoidingView>
     );
   }
@@ -27,8 +38,21 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
+  },
+  imageContainer: {
+    flex: 1,
+  },
+  image: {
+    flex: 1,
+    width: null,
+    height: null,
+    resizeMode: 'cover',
+  },
+  detailsContainer: {
+    flex: 1,
     justifyContent: 'center',
+    backgroundColor: 'rgba(0,0,0,0.2)',
+    paddingHorizontal: 20,
   },
   textStyle: {
     textAlign: 'center',
@@ -40,6 +64,7 @@ const styles = StyleSheet.create({
         fontFamily: 'Roboto',
       },
     }),
+    color: 'white',
   },
   largeText: {
     fontSize: 44,
